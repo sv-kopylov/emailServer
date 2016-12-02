4bdc2f047bb04ba61c1b81f024c42c0b9874c184
sv_kopylov <imber81@gmail.com>
2016-12-02 11:47:10 +0300

    created

---
+++ src/java/ru/kopylov/ejb/LogineFacade.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.kopylov.ejb;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import ru.kopylov.persist.PostBox;
+
+/**
+ *
+ * @author Сергей
+ */
+public class LogineFacade extends AbstractFacade<PostBox>{
+    
+    @PersistenceContext(unitName = "EmailServerPU")
+    private EntityManager em;
+
+    public LogineFacade(Class<PostBox> entityClass) {
+          super(PostBox.class);
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+}
